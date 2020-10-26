941ba9e132500d2502d94d722389d6e7f18f21c6
Nathan Ravenscraft <nraven22@gmail.com>
2020-10-26 16:44:01 -0400

    does this work now?

--- snippet/models.py
+++ snippet/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Snippet(models.Model):
+    """Represent a code snippet"""
+    title = models.CharField(max_length=255, blank = True, null = True)
+    body = models.TextField()
+    language = models.CharField(max_length=255) 
+    #author = models.ForeignKey(User, on_delete=Cascade)
+    #original_author = models.ForeignKey(Snippet, ??????)
+
+    
+
+
--- snippet/views.py
+++ snippet/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Snippet
+from django.contribu.auth import authenticate, login
+#from .forms import PoemForm, CommentForm, ContactForm, SearchForm
+
 
 # Create your views here.
+def snippet_list(request):
+    snippets = Snippet.objects.all()
+
+    return render(request, "snippet/snippet_list.html", {"snippets": snippets})
+
+
+# def login_view(request):
+#     username = request.POST['username']
+#     password = request.POST['password']
+#     user = authenticate(request, username=username, password=password)
+#     if user is not None:
+#         login(request, user)
+#         return render(request, "snippet/snippet_list.html", {"snippets": Snippet.objects.filter(user=user)})
+#     else:
+#         # Return an 'invalid login' error message.
+        
